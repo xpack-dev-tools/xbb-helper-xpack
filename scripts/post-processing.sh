5b1451311b6d4324b53a3f4c83210928e6ef0eed
Liviu Ionescu <ilg@livius.net>
2022-10-07 10:37:29 +0300

    post-processing: use $(ndate) in check-binaries

--- scripts/post-processing.sh
+++ scripts/post-processing.sh
@@ -2015,7 +2015,7 @@ function check_binaries()
       echo "Oops! Unsupported TARGET_PLATFORM=${TARGET_PLATFORM}."
       exit 1
     fi
-  ) 2>&1 | tee "${LOGS_FOLDER_PATH}/check-binaries-output-$(date -u +%Y%m%d-%H%M).txt"
+  ) 2>&1 | tee "${LOGS_FOLDER_PATH}/check-binaries-output-$(ndate).txt"
 }
 
 function check_binary()
