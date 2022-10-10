f58de761e4cbd2ddeda729f0ec238b112081821b
Liviu Ionescu <ilg@livius.net>
2022-10-10 15:13:10 +0300

    trigger-workflow-test-prime: cleanup

--- github-actions/trigger-workflow-test-prime.sh
+++ github-actions/trigger-workflow-test-prime.sh
@@ -52,7 +52,6 @@ source "${scripts_folder_path}/definitions.sh"
 
 # Helper functions
 source "${helper_folder_path}/github-actions/common.sh"
-source "${helper_folder_path}/scripts/xbb.sh"
 
 # -----------------------------------------------------------------------------
 
