3d897390959a2cd934cde3e8187811dbfd6e11f1
Liviu Ionescu <ilg@livius.net>
2022-11-02 16:09:11 +0000

    hidapi.sh: move copy_libudev to miscellaneous.sh

--- scripts/miscellaneous.sh
+++ scripts/miscellaneous.sh
@@ -130,3 +130,74 @@ function copy_cmake_logs()
 }
 
 # -----------------------------------------------------------------------------
+
+function copy_libudev()
+{
+  (
+    if [ -f "/usr/include/libudev.h" ]
+    then
+      cp -v "/usr/include/libudev.h" "${XBB_LIBRARIES_INSTALL_FOLDER_PATH}/include"
+    else
+      echo "No libudev.h"
+      exit 1
+    fi
+
+    local find_path="$(find /lib* -name 'libudev.so.?' | sort -u | sed -n 1p)"
+    if [ ! -z "${find_path}" ]
+    then
+      copy_libudev_with_links "${find_path}" "${XBB_LIBRARIES_INSTALL_FOLDER_PATH}/lib"
+    fi
+
+    find_path="$(find /usr/lib* -name 'libudev.so' | sort -u | sed -n 1p)"
+    if [ ! -z "${find_path}" ]
+    then
+      copy_libudev_with_links "${find_path}" "${XBB_LIBRARIES_INSTALL_FOLDER_PATH}/lib"
+    fi
+
+    local find_pc_path="$(find /usr/lib* -name 'libudev.pc')"
+    if [ ! -z "${find_pc_path}" ]
+    then
+      cp -v "${find_pc_path}" "${XBB_LIBRARIES_INSTALL_FOLDER_PATH}/lib/pkgconfig"
+    else
+      echo "No libudev.pc"
+      exit 1
+    fi
+  )
+}
+
+function copy_libudev_with_links()
+{
+  local from_file_path="$1"
+  local dest_folder_path="$2"
+
+  if [ -L "${from_file_path}" ]
+  then
+    local link_file_path="$(readlink "${from_file_path}")"
+    if [ "${link_file_path}" == "$(basename "${link_file_path}")" ]
+    then
+      copy_libudev_with_links "$(dirname "${from_file_path}")/${link_file_path}" "${dest_folder_path}"
+    else
+      copy_libudev_with_links "${link_file_path}" "${dest_folder_path}"
+    fi
+    (
+      cd "${dest_folder_path}"
+      if [ ! -L "$(basename "${from_file_path}")" ]
+      then
+        ln -sv "$(basename "${link_file_path}")" "$(basename "${from_file_path}")"
+      fi
+    )
+  else
+    local dest_file_path="${dest_folder_path}/$(basename "${from_file_path}")"
+    if [ ! -f "${dest_file_path}" ]
+    then
+      cp -v "${from_file_path}" "${dest_folder_path}"
+    fi
+
+    # Hack to get libudev.so in line with the 'all rpath' policy,
+    # since on arm 32-bit it is checked.
+    # Manually add $ORIGIN to libudev.so (fingers crossed!).
+    run_verbose ${PATCHELF:-$(which patchelf || echo patchelf)} --force-rpath --set-rpath "\$ORIGIN" "${dest_file_path}"
+  fi
+}
+
+# -----------------------------------------------------------------------------
