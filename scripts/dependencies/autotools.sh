eff1293f841e419543121ac9004a94583d1a8c58
Liviu Ionescu <ilg@livius.net>
2022-11-04 11:34:34 +0200

    add autotools.sh

---
+++ scripts/dependencies/autotools.sh
@@ -0,0 +1,41 @@
+# -----------------------------------------------------------------------------
+# This file is part of the xPack distribution.
+#   (https://xpack.github.io)
+# Copyright (c) 2020 Liviu Ionescu.
+#
+# Permission to use, copy, modify, and/or distribute this software
+# for any purpose is hereby granted, under the terms of the MIT license.
+# -----------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------
+# This is a shortcut to build the latest versions of all autotools.
+
+for dependency in libiconv autoconf automake libtool pkg-config
+do
+  echo "Including ${helper_folder_path}/scripts/dependencies/${dependency}.sh..."
+  source "${helper_folder_path}/scripts/dependencies/${dependency}.sh"
+done
+
+function build_autotools()
+{
+  # https://ftp.gnu.org/pub/gnu/libiconv/
+  build_libiconv "1.17" # "1.16"
+
+  # https://ftp.gnu.org/gnu/autoconf/
+  # depends on m4.
+  build_autoconf "2.71"
+
+  # https://ftp.gnu.org/gnu/automake/
+  # depends on autoconf.
+  build_automake "1.16.5"
+
+  # http://ftpmirror.gnu.org/libtool/
+  build_libtool "2.4.7"
+
+  # configure.ac:34: error: Macro PKG_PROG_PKG_CONFIG is not available. It is usually defined in file pkg.m4 provided by package pkg-config.
+  # https://pkgconfig.freedesktop.org/releases/
+  # depends on libiconv
+  build_pkg_config "0.29.2"
+}
+
+# -----------------------------------------------------------------------------
