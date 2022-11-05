85e6993a263dfd55064d697148a332896733d7f9
Liviu Ionescu <ilg@livius.net>
2022-11-05 12:21:09 +0200

    extract gpm.sh

---
+++ scripts/dependencies/gpm.sh
@@ -0,0 +1,176 @@
+# -----------------------------------------------------------------------------
+# This file is part of the xPack distribution.
+#   (https://xpack.github.io)
+# Copyright (c) 2020 Liviu Ionescu.
+#
+# Permission to use, copy, modify, and/or distribute this software
+# for any purpose is hereby granted, under the terms of the MIT license.
+# -----------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------
+
+function build_gpm()
+{
+  # General purpose mouse. Used by ncurses.
+  # https://www.nico.schottelius.org/software/gpm/
+  # https://github.com/telmich/gpm
+  # https://github.com/telmich/gpm/tags
+  # https://github.com/telmich/gpm/releases/tag/1.20.7
+  # https://github.com/telmich/gpm/archive/1.20.7.tar.gz
+  # https://github.com/xpack-dev-tools/gpm/archive/refs/tags/v1.20.7-1.tar.gz
+
+  # https://archlinuxarm.org/packages/aarch64/gpm/files/PKGBUILD
+
+  # 27 Oct 2012 "1.20.7"
+  # 25 Apr 2022 "1.20.7-1" xPack
+
+  local gpm_version="$1"
+
+  local gpm_src_folder_name="gpm-${gpm_version}"
+
+  local gpm_archive="${gpm_src_folder_name}.tar.gz"
+  # GitHub release archive.
+  local gpm_github_archive="${gpm_version}.tar.gz"
+  # local gpm_github_url="https://github.com/telmich/gpm/archive/${gpm_github_archive}"
+  local gpm_github_url="https://github.com/xpack-dev-tools/gpm/archive/refs/tags/v${gpm_github_archive}"
+
+  local gpm_folder_name="${gpm_src_folder_name}"
+
+  mkdir -pv "${XBB_LOGS_FOLDER_PATH}/${gpm_folder_name}"
+
+  local gpm_stamp_file_path="${XBB_STAMPS_FOLDER_PATH}/stamp-${gpm_folder_name}-installed"
+  if [ ! -f "${gpm_stamp_file_path}" ]
+  then
+
+    echo
+    echo "gmp in-source building..."
+
+    mkdir -pv "${XBB_BUILD_FOLDER_PATH}"
+    cd "${XBB_BUILD_FOLDER_PATH}"
+
+    if [ ! -d "${XBB_BUILD_FOLDER_PATH}/${gpm_folder_name}" ]
+    then
+      download_and_extract "${gpm_github_url}" "${gpm_archive}" \
+        "${gpm_src_folder_name}"
+
+      if [ "${gpm_src_folder_name}" != "${gpm_folder_name}" ]
+      then
+        mv -v "${gpm_src_folder_name}" "${gpm_folder_name}"
+      fi
+    fi
+
+    (
+      cd "${XBB_BUILD_FOLDER_PATH}/${gpm_folder_name}"
+      if [ ! -f "stamp-autogen" ]
+      then
+
+        run_verbose bash ${DEBUG} "autogen.sh"
+
+        touch "stamp-autogen"
+      fi
+    ) 2>&1 | tee "${XBB_LOGS_FOLDER_PATH}/${gpm_folder_name}/autogen-output-$(ndate).txt"
+
+    (
+      cd "${XBB_BUILD_FOLDER_PATH}/${gpm_folder_name}"
+
+      xbb_activate_installed_dev
+
+      CPPFLAGS="${XBB_CPPFLAGS} -I${XBB_LIBRARIES_INSTALL_FOLDER_PATH}/${gpm_folder_name}/src/headers"
+      CFLAGS="${XBB_CFLAGS_NO_W}"
+      CXXFLAGS="${XBB_CXXFLAGS_NO_W}"
+
+      LDFLAGS="${XBB_LDFLAGS_LIB}"
+      if [ "${XBB_TARGET_PLATFORM}" == "linux" ]
+      then
+        xbb_activate_cxx_rpath
+        LDFLAGS+=" -Wl,-rpath,${LD_LIBRARY_PATH}"
+        LDFLAGS+=" -Wl,--allow-multiple-definition"
+      fi
+
+      export CPPFLAGS
+      export CFLAGS
+      export CXXFLAGS
+      export LDFLAGS
+
+      if [ ! -f "config.status" ]
+      then
+        (
+          xbb_show_env_develop
+
+          echo
+          echo "Running gpm configure..."
+
+          if [ "${XBB_IS_DEVELOP}" == "y" ]
+          then
+            run_verbose bash "configure" --help
+          fi
+
+          config_options=()
+
+          config_options+=("--prefix=${XBB_BINARIES_INSTALL_FOLDER_PATH}")
+          config_options+=("--libdir=${XBB_LIBRARIES_INSTALL_FOLDER_PATH}/lib")
+          config_options+=("--includedir=${XBB_LIBRARIES_INSTALL_FOLDER_PATH}/include")
+          # config_options+=("--datarootdir=${XBB_LIBRARIES_INSTALL_FOLDER_PATH}/share")
+          config_options+=("--mandir=${XBB_LIBRARIES_INSTALL_FOLDER_PATH}/share/man")
+
+          config_options+=("--build=${XBB_BUILD}")
+          config_options+=("--host=${XBB_HOST}")
+          config_options+=("--target=${XBB_TARGET}")
+
+          # config_options+=("--with-pic")
+
+          run_verbose bash ${DEBUG} "configure" \
+            "${config_options[@]}"
+
+          cp "config.log" "${XBB_LOGS_FOLDER_PATH}/${gpm_folder_name}/config-log-$(ndate).txt"
+        ) 2>&1 | tee "${XBB_LOGS_FOLDER_PATH}/${gpm_folder_name}/configure-output-$(ndate).txt"
+      fi
+
+      (
+        echo
+        echo "Running gpm make..."
+
+        # Build.
+        run_verbose make -j ${XBB_JOBS}
+
+        if [ "${XBB_WITH_TESTS}" == "y" ]
+        then
+          run_verbose make -j1 check
+        fi
+
+        if [ "${XBB_WITH_STRIP}" == "y" ]
+        then
+          run_verbose make install-strip
+        else
+          run_verbose make install
+        fi
+
+        if [ "${XBB_TARGET_PLATFORM}" == "linux" ]
+        then
+          (
+            mkdir -pv "${XBB_BINARIES_INSTALL_FOLDER_PATH}/bin"
+            cd "${XBB_BINARIES_INSTALL_FOLDER_PATH}/bin"
+
+            # Manual copy, since it is not refered in the elf.
+            cp -v "${XBB_LIBRARIES_INSTALL_FOLDER_PATH}/lib/libgpm.so.2.1.0" .
+            rm -f "libgpm.so.2"
+            ln -s -v "libgpm.so.2.1.0" "libgpm.so.2"
+          )
+        fi
+
+      ) 2>&1 | tee "${XBB_LOGS_FOLDER_PATH}/${gpm_folder_name}/make-output-$(ndate).txt"
+
+      copy_license \
+        "${XBB_BUILD_FOLDER_PATH}/${gpm_folder_name}" \
+        "${gpm_folder_name}"
+    )
+
+    mkdir -pv "${XBB_STAMPS_FOLDER_PATH}"
+    touch "${gpm_stamp_file_path}"
+
+  else
+    echo "Library gpm already installed."
+  fi
+}
+
+# -----------------------------------------------------------------------------
