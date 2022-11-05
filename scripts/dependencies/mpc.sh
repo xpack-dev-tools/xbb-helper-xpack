f8787644cf26297de33c17398f258935e7765542
Liviu Ionescu <ilg@livius.net>
2022-11-05 12:19:34 +0200

    extract mpc.sh

---
+++ scripts/dependencies/mpc.sh
@@ -0,0 +1,177 @@
+# -----------------------------------------------------------------------------
+# This file is part of the xPack distribution.
+#   (https://xpack.github.io)
+# Copyright (c) 2020 Liviu Ionescu.
+#
+# Permission to use, copy, modify, and/or distribute this software
+# for any purpose is hereby granted, under the terms of the MIT license.
+# -----------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------
+
+# Depends on gmp, mpfr.
+function build_mpc()
+{
+  # http://www.multiprecision.org/
+  # ftp://ftp.gnu.org/gnu/mpc
+
+  # https://github.com/archlinux/svntogit-packages/blob/packages/mpc/trunk/PKGBUILD
+  # https://archlinuxarm.org/packages/aarch64/mpc/files/PKGBUILD
+
+  # https://github.com/Homebrew/homebrew-core/blob/master/Formula/mpc.rb
+
+  # 20 Feb 2015 "1.0.3"
+  # 2018-01-11 "1.1.0"
+  # 2020-08 "1.2.0"
+  # 2020-10 "1.2.1"
+
+  local mpc_version="$1"
+  local name_suffix=${2-''}
+
+  # The folder name as resulted after being extracted from the archive.
+  local mpc_src_folder_name="mpc-${mpc_version}"
+
+  local mpc_archive="${mpc_src_folder_name}.tar.gz"
+  local mpc_url="ftp://ftp.gnu.org/gnu/mpc/${mpc_archive}"
+  if [[ ${mpc_version} =~ 0\.* ]]
+  then
+    mpc_url="http://www.multiprecision.org/downloads/${mpc_archive}"
+  fi
+
+  # The folder name for build, licenses, etc.
+  local mpc_folder_name="${mpc_src_folder_name}${name_suffix}"
+
+  mkdir -pv "${XBB_LOGS_FOLDER_PATH}/${mpc_folder_name}"
+
+  local mpc_stamp_file_path="${XBB_STAMPS_FOLDER_PATH}/stamp-${mpc_folder_name}-installed"
+  if [ ! -f "${mpc_stamp_file_path}" ]
+  then
+
+    mkdir -pv "${XBB_SOURCES_FOLDER_PATH}"
+    cd "${XBB_SOURCES_FOLDER_PATH}"
+
+    download_and_extract "${mpc_url}" "${mpc_archive}" \
+      "${mpc_src_folder_name}"
+
+    (
+      mkdir -pv "${XBB_BUILD_FOLDER_PATH}/${mpc_folder_name}"
+      cd "${XBB_BUILD_FOLDER_PATH}/${mpc_folder_name}"
+
+      if [ "${name_suffix}" == "${XBB_BOOTSTRAP_SUFFIX}" ]
+      then
+
+        CPPFLAGS="${XBB_CPPFLAGS}"
+        CFLAGS="${XBB_CFLAGS_NO_W}"
+        CXXFLAGS="${XBB_CXXFLAGS_NO_W}"
+
+        LDFLAGS="${XBB_LDFLAGS_LIB}"
+
+      else
+
+        xbb_activate_installed_dev
+
+        CPPFLAGS="${XBB_CPPFLAGS}"
+        CFLAGS="${XBB_CFLAGS_NO_W}"
+        CXXFLAGS="${XBB_CXXFLAGS_NO_W}"
+
+        LDFLAGS="${XBB_LDFLAGS_LIB}"
+        if [ "${XBB_TARGET_PLATFORM}" == "linux" ]
+        then
+          xbb_activate_cxx_rpath
+          LDFLAGS+=" -Wl,-rpath,${LD_LIBRARY_PATH}"
+        fi
+
+      fi
+
+      export CPPFLAGS
+      export CFLAGS
+      export CXXFLAGS
+      export LDFLAGS
+
+      if [ ! -f "config.status" ]
+      then
+        (
+          xbb_show_env_develop
+
+          echo
+          echo "Running mpc${name_suffix} configure..."
+
+          if [ "${XBB_IS_DEVELOP}" == "y" ]
+          then
+            run_verbose bash "${XBB_SOURCES_FOLDER_PATH}/${mpc_src_folder_name}/configure" --help
+          fi
+
+          config_options=()
+
+          config_options+=("--prefix=${XBB_BINARIES_INSTALL_FOLDER_PATH}${name_suffix}")
+          config_options+=("--libdir=${XBB_LIBRARIES_INSTALL_FOLDER_PATH}${name_suffix}/lib")
+          config_options+=("--includedir=${XBB_LIBRARIES_INSTALL_FOLDER_PATH}${name_suffix}/include")
+          # config_options+=("--datarootdir=${XBB_LIBRARIES_INSTALL_FOLDER_PATH}${name_suffix}/share")
+          config_options+=("--mandir=${XBB_LIBRARIES_INSTALL_FOLDER_PATH}${name_suffix}/share/man")
+
+          if [ "${name_suffix}" == "${XBB_BOOTSTRAP_SUFFIX}" ]
+          then
+
+            config_options+=("--build=${XBB_BUILD}")
+            config_options+=("--host=${XBB_BUILD}")
+            config_options+=("--target=${XBB_BUILD}")
+
+          else
+
+            config_options+=("--build=${XBB_BUILD}")
+            config_options+=("--host=${XBB_HOST}")
+            config_options+=("--target=${XBB_TARGET}")
+
+          fi
+
+          config_options+=("--disable-maintainer-mode")
+
+          config_options+=("--with-gmp=${XBB_LIBRARIES_INSTALL_FOLDER_PATH}${name_suffix}")
+          config_options+=("--with-mpfr=${XBB_LIBRARIES_INSTALL_FOLDER_PATH}${name_suffix}")
+
+          run_verbose bash ${DEBUG} "${XBB_SOURCES_FOLDER_PATH}/${mpc_src_folder_name}/configure" \
+            "${config_options[@]}"
+
+          cp "config.log" "${XBB_LOGS_FOLDER_PATH}/${mpc_folder_name}/config-log-$(ndate).txt"
+        ) 2>&1 | tee "${XBB_LOGS_FOLDER_PATH}/${mpc_folder_name}/configure-output-$(ndate).txt"
+      fi
+
+      (
+        echo
+        echo "Running mpc${name_suffix} make..."
+
+        # Build.
+        run_verbose make -j ${XBB_JOBS}
+
+        if [ "${XBB_WITH_TESTS}" == "y" ]
+        then
+          run_verbose make -j1 check
+        fi
+
+        if [ "${XBB_WITH_STRIP}" == "y" ]
+        then
+          run_verbose make install-strip
+        else
+          run_verbose make install
+        fi
+
+      ) 2>&1 | tee "${XBB_LOGS_FOLDER_PATH}/${mpc_folder_name}/make-output-$(ndate).txt"
+
+      if [ -z "${name_suffix}" ]
+      then
+        copy_license \
+          "${XBB_SOURCES_FOLDER_PATH}/${mpc_src_folder_name}" \
+          "${mpc_folder_name}"
+      fi
+
+    )
+
+    mkdir -pv "${XBB_STAMPS_FOLDER_PATH}"
+    touch "${mpc_stamp_file_path}"
+
+  else
+    echo "Library mpc${name_suffix} already installed."
+  fi
+}
+
+# -----------------------------------------------------------------------------
