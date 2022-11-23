7b6fc8b04e8170f94200c6402aa4987cf4c13832
Liviu Ionescu <ilg@livius.net>
2022-11-23 23:17:51 +0200

    show-libs.sh: separate host from target libs

--- scripts/show-libs.sh
+++ scripts/show-libs.sh
@@ -10,56 +10,46 @@
 
 # -----------------------------------------------------------------------------
 
-function show_libs()
+function show_host_libs()
 {
-  # Does not include the .exe extension.
   local app_path=$1
   shift
 
   (
-    if [ "${XBB_HOST_PLATFORM}" == "linux" ]
+    if [ ! -f "${app_path}" ]
+    then
+      run_verbose file "${app_path}"
+      return
+    fi
+
+    # Does not need to include the .exe extension.
+    if is_pe "${app_path}"
     then
       run_verbose ls -l "${app_path}"
       if [ "${XBB_IS_DEVELOP}" == "y" ]
       then
-        run_verbose file -L "${app_path}"
+        run_verbose file "${app_path}"
       fi
       echo
-      echo "[readelf -d ${app_path} | egrep ...]"
-      # Ignore errors in case it is not using shared libraries.
-      set +e
-      readelf_shared_libs "${app_path}"
-      echo
-      echo "[ldd -v ${app_path}]"
-      ldd -v "${app_path}" || true
-      set -e
-    elif [ "${XBB_HOST_PLATFORM}" == "darwin" ]
+      echo "[${XBB_HOST_OBJDUMP} -x ${app_path}]"
+      "${XBB_HOST_OBJDUMP}" -x "${app_path}" | grep -i 'DLL Name' || true
+    elif is_pe "${app_path}.exe"
     then
-      run_verbose ls -l "${app_path}"
+      run_verbose ls -l "${app_path}.exe"
       if [ "${XBB_IS_DEVELOP}" == "y" ]
       then
-        run_verbose file -L "${app_path}"
+        run_verbose file "${app_path}.exe"
       fi
       echo
-      echo "[otool -L ${app_path}]"
-      set +e
-      local lc_rpaths=$(get_darwin_lc_rpaths "${app_path}")
-      local lc_rpaths_line=$(echo "${lc_rpaths}" | tr '\n' ':' | sed -e 's|:$||')
-      if [ ! -z "${lc_rpaths_line}" ]
-      then
-        echo "${app_path}: (LC_RPATH=${lc_rpaths_line})"
-      else
-        echo "${app_path}:"
-      fi
-      otool -L "${app_path}" | tail -n +2
-    elif [ "${XBB_HOST_PLATFORM}" == "win32" ]
-    then
-      if is_elf "${app_path}"
+      echo "[${XBB_HOST_OBJDUMP} -x ${app_path}.exe]"
+      "${XBB_HOST_OBJDUMP}" -x "${app_path}.exe" | grep -i 'DLL Name' || true
+    else
+      if [ "${XBB_BUILD_PLATFORM}" == "linux" ]
       then
         run_verbose ls -l "${app_path}"
         if [ "${XBB_IS_DEVELOP}" == "y" ]
         then
-          run_verbose file "${app_path}"
+          run_verbose file -L "${app_path}"
         fi
         echo
         echo "[readelf -d ${app_path} | egrep ...]"
@@ -70,37 +60,121 @@ function show_libs()
         echo "[ldd -v ${app_path}]"
         ldd -v "${app_path}" || true
         set -e
-      else
-        if false # [ -f "${app_path}" ]
+      elif [ "${XBB_BUILD_PLATFORM}" == "darwin" ]
+      then
+        run_verbose ls -l "${app_path}"
+        if [ "${XBB_IS_DEVELOP}" == "y" ]
         then
-          run_verbose ls -l "${app_path}"
-          if [ "${XBB_IS_DEVELOP}" == "y" ]
-          then
-            run_verbose file "${app_path}"
-          fi
-          echo
-          echo "[${XBB_TARGET_TRIPLET}-objdump -x ${app_path}]"
-          ${XBB_TARGET_TRIPLET}-objdump -x ${app_path} | grep -i 'DLL Name' || true
-        elif [ -f "${app_path}.exe" ]
-        then
-          run_verbose ls -l "${app_path}.exe"
-          if [ "${XBB_IS_DEVELOP}" == "y" ]
-          then
-            run_verbose file "${app_path}.exe"
-          fi
-          echo
-          echo "[${XBB_TARGET_TRIPLET}-objdump -x ${app_path}.exe]"
-          ${XBB_TARGET_TRIPLET}-objdump -x ${app_path}.exe | grep -i 'DLL Name' || true
-        else
-          run_verbose file "${app_path}"
+          run_verbose file -L "${app_path}"
         fi
+        echo
+        echo "[otool -L ${app_path}]"
+        set +e
+        local lc_rpaths=$(get_darwin_lc_rpaths "${app_path}")
+        local lc_rpaths_line=$(echo "${lc_rpaths}" | tr '\n' ':' | sed -e 's|:$||')
+        if [ ! -z "${lc_rpaths_line}" ]
+        then
+          echo "${app_path}: (LC_RPATH=${lc_rpaths_line})"
+        else
+          echo "${app_path}:"
+        fi
+        otool -L "${app_path}" | tail -n +2
+      else
+        run_verbose ls -l "${app_path}"
+        if [ "${XBB_IS_DEVELOP}" == "y" ]
+        then
+          run_verbose file -L "${app_path}"
+        fi
+        echo "Unsupported XBB_BUILD_PLATFORM=${XBB_BUILD_PLATFORM} in ${FUNCNAME[0]}()"
+        exit 1
       fi
-    else
-      echo "Unsupported XBB_HOST_PLATFORM=${XBB_HOST_PLATFORM} in ${FUNCNAME[0]}()"
-      exit 1
     fi
   )
 }
+
+function show_target_libs()
+{
+  local app_path=$1
+  shift
+
+  (
+    if [ ! -f "${app_path}" ]
+    then
+      run_verbose file "${app_path}"
+      return
+    fi
+
+    # Does not need to include the .exe extension.
+    if is_pe "${app_path}"
+    then
+      run_verbose ls -l "${app_path}"
+      if [ "${XBB_IS_DEVELOP}" == "y" ]
+      then
+        run_verbose file "${app_path}"
+      fi
+      echo
+      echo "[${XBB_TARGET_OBJDUMP} -x ${app_path}]"
+      "${XBB_TARGET_OBJDUMP}" -x "${app_path}" | grep -i 'DLL Name' || true
+    elif is_pe "${app_path}.exe"
+    then
+      run_verbose ls -l "${app_path}.exe"
+      if [ "${XBB_IS_DEVELOP}" == "y" ]
+      then
+        run_verbose file "${app_path}.exe"
+      fi
+      echo
+      echo "[${XBB_TARGET_OBJDUMP} -x ${app_path}.exe]"
+      "${XBB_TARGET_OBJDUMP}" -x "${app_path}.exe" | grep -i 'DLL Name' || true
+    else
+      if [ "${XBB_BUILD_PLATFORM}" == "linux" ]
+      then
+        run_verbose ls -l "${app_path}"
+        if [ "${XBB_IS_DEVELOP}" == "y" ]
+        then
+          run_verbose file -L "${app_path}"
+        fi
+        echo
+        echo "[readelf -d ${app_path} | egrep ...]"
+        # Ignore errors in case it is not using shared libraries.
+        set +e
+        readelf_shared_libs "${app_path}"
+        echo
+        echo "[ldd -v ${app_path}]"
+        ldd -v "${app_path}" || true
+        set -e
+      elif [ "${XBB_BUILD_PLATFORM}" == "darwin" ]
+      then
+        run_verbose ls -l "${app_path}"
+        if [ "${XBB_IS_DEVELOP}" == "y" ]
+        then
+          run_verbose file -L "${app_path}"
+        fi
+        echo
+        echo "[otool -L ${app_path}]"
+        set +e
+        local lc_rpaths=$(get_darwin_lc_rpaths "${app_path}")
+        local lc_rpaths_line=$(echo "${lc_rpaths}" | tr '\n' ':' | sed -e 's|:$||')
+        if [ ! -z "${lc_rpaths_line}" ]
+        then
+          echo "${app_path}: (LC_RPATH=${lc_rpaths_line})"
+        else
+          echo "${app_path}:"
+        fi
+        otool -L "${app_path}" | tail -n +2
+      else
+        run_verbose ls -l "${app_path}"
+        if [ "${XBB_IS_DEVELOP}" == "y" ]
+        then
+          run_verbose file -L "${app_path}"
+        fi
+        echo "Unsupported XBB_BUILD_PLATFORM=${XBB_BUILD_PLATFORM} in ${FUNCNAME[0]}()"
+        exit 1
+      fi
+    fi
+  )
+}
+
+# -----------------------------------------------------------------------------
 
 function readelf_shared_libs()
 {
