664356f2ea70d414c9676f4e4b43e945e3dbd512
Liviu Ionescu <ilg@livius.net>
2022-12-08 14:48:08 +0200

    timer.sh: remove trailing dots in messages

--- scripts/timer.sh
+++ scripts/timer.sh
@@ -14,7 +14,7 @@ function timer_start()
 {
   export XBB_SCRIPT_BEGIN_SECOND=$(date +%s)
   echo
-  echo "Script \"$0\" started at $(date)."
+  echo "Script \"$0\" started at $(date)"
 }
 
 function timer_stop()
@@ -22,20 +22,20 @@ function timer_stop()
   local script_end_second=$(date +%s)
   echo
   echo "The ${XBB_APPLICATION_DISTRO_NAME} ${XBB_APPLICATION_NAME} (${XBB_APPLICATION_LOWER_CASE_NAME}) project"
-  echo "Script \"$0\" completed at $(date)."
+  echo "Script \"$0\" completed at $(date)"
   local delta_seconds=$((script_end_second-XBB_SCRIPT_BEGIN_SECOND))
   if [ ${delta_seconds} -lt 100 ]
   then
-    echo "Duration: ${delta_seconds} second(s)."
+    echo "Duration: ${delta_seconds} second(s)"
   elif [ ${delta_seconds} -lt 3600 ]
   then
     local delta_minutes=$(((delta_seconds+30)/60))
-    echo "Duration: ${delta_minutes} minute(s)."
+    echo "Duration: ${delta_minutes} minute(s)"
   else
     local delta_minutes=$(((delta_seconds+30)/60))
     local delta_hours=$((delta_minutes/60))
     local delta_hour_minutes=$((delta_minutes-(delta_hours*60)))
-    printf "Duration: %dh%02dm.\n" ${delta_hours} ${delta_hour_minutes}
+    printf "Duration: %dh%02dm\n" ${delta_hours} ${delta_hour_minutes}
   fi
 }
 
