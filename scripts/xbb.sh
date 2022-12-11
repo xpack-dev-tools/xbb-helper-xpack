4e4e0d83723b469ea72b9e383e90ce57f833a119
Liviu Ionescu <ilg@livius.net>
2022-12-11 20:28:58 +0200

    xbb.sh: fix PYTHON definition; add M4 & SED

--- scripts/xbb.sh
+++ scripts/xbb.sh
@@ -50,7 +50,9 @@ function xbb_save_env()
   export XBB_SAVED_PATH="${PATH:-""}"
   export XBB_SAVED_LD_LIBRARY_PATH="${LD_LIBRARY_PATH:-""}"
 
-  export PYTHON=$(which python || python)
+  export M4=$(which gm4 || which m4 || echo m4)
+  export PYTHON=$(which python || echo python)
+  export SED=$(which gsed || which sed || echo sed)
 }
 
 function xbb_reset_env()
