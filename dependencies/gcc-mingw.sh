58155ee2eae47b7dde27d97b266d51ffb859a134
Liviu Ionescu <ilg@livius.net>
2023-01-01 18:47:09 +0200

    gcc-mingw.sh: fix ZSTD with -lpthread

--- dependencies/gcc-mingw.sh
+++ dependencies/gcc-mingw.sh
@@ -191,21 +191,26 @@ function gcc_mingw_build_first()
         CXXFLAGS+=" -D__USE_MINGW_ACCESS"
       fi
 
-      if is_native || is_bootstrap
-      then
-        # Hack to avoid missing ZSTD_* symbols
-        # /home/ilg/.local/xPacks/@xpack-dev-tools/gcc/12.2.0-2.1/.content/bin/../lib/gcc/x86_64-pc-linux-gnu/12.2.0/../../../../x86_64-pc-linux-gnu/bin/ld: lto-compress.o: in function `lto_end_compression(lto_compression_stream*)':
-        # lto-compress.cc:(.text._Z19lto_end_compressionP22lto_compression_stream+0x33): undefined reference to `ZSTD_compressBound'
-
-        export LIBS="-lzstd -lpthread"
-      fi
-
       # LDFLAGS="${XBB_LDFLAGS_APP_STATIC_GCC}"
       LDFLAGS="${XBB_LDFLAGS_APP}"
       xbb_adjust_ldflags_rpath
 
-      CFLAGS_FOR_TARGET="-g -ffunction-sections -fdata-sections -pipe -O2 -D__USE_MINGW_ACCESS -w"
-      CXXFLAGS_FOR_TARGET="-g -ffunction-sections -fdata-sections -pipe -O2 -D__USE_MINGW_ACCESS -w"
+      if [ "${XBB_HOST_PLATFORM}" == "linux" ]
+      then
+        if is_native || is_bootstrap
+        then
+          # Hack to avoid missing ZSTD_* symbols
+          # /home/ilg/.local/xPacks/@xpack-dev-tools/gcc/12.2.0-2.1/.content/bin/../lib/gcc/x86_64-pc-linux-gnu/12.2.0/../../../../x86_64-pc-linux-gnu/bin/ld: lto-compress.o: in function `lto_end_compression(lto_compression_stream*)':
+          # lto-compress.cc:(.text._Z19lto_end_compressionP22lto_compression_stream+0x33): undefined reference to `ZSTD_compressBound'
+
+          # Testing -lzstd alone fails since it depends on -lpthread.
+          LDFLAGS+=" -lpthread"
+          # export LIBS="-lzstd -lpthread"
+        fi
+      fi
+
+      export CFLAGS_FOR_TARGET="-g -ffunction-sections -fdata-sections -pipe -O2 -D__USE_MINGW_ACCESS -w"
+      export CXXFLAGS_FOR_TARGET="-g -ffunction-sections -fdata-sections -pipe -O2 -D__USE_MINGW_ACCESS -w"
 
       export CPPFLAGS
       export CFLAGS
