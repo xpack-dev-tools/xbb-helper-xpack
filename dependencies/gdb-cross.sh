956025fb891060620697867d4f1769a633bfb8da
Liviu Ionescu <ilg@livius.net>
2023-08-18 13:45:10 +0300

    gdb-cross.sh: libunistring_build

--- dependencies/gdb-cross.sh
+++ dependencies/gdb-cross.sh
@@ -24,6 +24,15 @@ function gdb_cross_build_dependencies()
   # Fails on mingw. 0.8.13 is deprecated. Not used anyway.
   # libelf_build "0.8.13"
 
+  # Undefined symbols for architecture x86_64:
+  #   "_gl_get_setlocale_null_lock", referenced from:
+  #       _libgettextpo_setlocale_null_r in setlocale_null.o
+  # ld: symbol(s) not found for architecture x86_64
+  # clang-16: error: linker command failed with exit code 1 (use -v to see invocation)
+
+  # https://ftp.gnu.org/gnu/libunistring/
+  libunistring_build "${XBB_LIBUNISTRING_VERSION}"
+
   # https://ftp.gnu.org/pub/gnu/gettext/
   gettext_build "${XBB_GETTEXT_VERSION}"
 
