110e29c2e8799532fc054c06e5eafccc480bd8b9
Liviu Ionescu <ilg@livius.net>
2023-08-20 15:19:29 +0300

    test-common.sh: install g++ for tests

--- scripts/test-common.sh
+++ scripts/test-common.sh
@@ -250,19 +250,43 @@ function tests_update_system_common()
   # Make sure that the minimum prerequisites are met.
   if [[ ${image_name} == github-actions-ubuntu* ]]
   then
-    : # sudo apt-get -qq install -y XXX
+    run_verbose sudo apt-get update
+    # To make 32-bit tests possible.
+    run_verbose sudo apt-get -qq install --yes g++-multilib
+  elif [[ ${image_name} == *raspbian* ]]
+  then
+    run_verbose apt-get -qq update
+    run_verbose apt-get -qq install --yes g++
   elif [[ ${image_name} == *ubuntu* ]] || [[ ${image_name} == *debian* ]] || [[ ${image_name} == *raspbian* ]]
   then
     run_verbose apt-get -qq update
     run_verbose apt-get -qq install -y git-core curl tar gzip lsb-release binutils file
+    
+    run_verbose apt-get -qq install --yes g++
+    if [ "$(uname -m)" == "x86_64" ]
+    then
+      run_verbose apt-get -qq install --yes g++-multilib
+    fi
   elif [[ ${image_name} == *centos* ]] || [[ ${image_name} == *redhat* ]] || [[ ${image_name} == *fedora* ]]
   then
     run_verbose yum update --assumeyes --quiet
     run_verbose yum install --assumeyes --quiet git curl tar gzip redhat-lsb-core binutils which
+
+    run_verbose yum install --assumeyes --quiet gcc-c++ glibc glibc-common libstdc++ libatomic
+    if [ "$(uname -m)" == "x86_64" ]
+    then
+      run_verbose yum install --assumeyes --quiet libgcc*i686 libstdc++*i686 glibc*i686 libatomic*i686 # libgfortran*i686
+    fi
   elif [[ ${image_name} == *suse* ]]
   then
     run_verbose zypper --quiet --no-gpg-checks update --no-confirm
     run_verbose zypper --quiet --no-gpg-checks install --no-confirm git-core curl tar gzip lsb-release binutils findutils util-linux
+
+    run_verbose zypper --quiet --no-gpg-checks install --no-confirm gcc-c++ glibc
+    if [ "$(uname -m)" == "x86_64" ]
+    then
+      run_verbose zypper --quiet --no-gpg-checks install --no-confirm gcc-32bit
+    fi
   elif [[ ${image_name} == *manjaro* ]]
   then
     # run_verbose pacman-mirrors -g
@@ -271,6 +295,12 @@ function tests_update_system_common()
     # Update even if up to date (-yy) & upgrade (-u).
     # pacman -S -yy -u -q --noconfirm
     run_verbose pacman -S --quiet --noconfirm --noprogressbar git curl tar gzip lsb-release binutils which
+
+    run_verbose pacman -S --quiet --noconfirm --noprogressbar gcc
+    if [ "$(uname -m)" == "x86_64" ]
+    then
+      run_verbose pacman -S --quiet --noconfirm --noprogressbar lib32-gcc-libs
+    fi
   elif [[ ${image_name} == *archlinux* ]]
   then
     run_verbose pacman -S --refresh --quiet --noconfirm --sysupgrade
@@ -278,7 +308,17 @@ function tests_update_system_common()
     # Update even if up to date (-yy) & upgrade (-u).
     # pacman -S -yy -u -q --noconfirm
     run_verbose pacman -S --quiet --noconfirm --noprogressbar git curl tar gzip lsb-release binutils which
+
+    run_verbose pacman -S --quiet --noconfirm --noprogressbar gcc
+    if [ "$(uname -m)" == "x86_64" ]
+    then
+      run_verbose pacman -S --quiet --noconfirm --noprogressbar lib32-gcc-libs
+    fi
   fi
+
+  echo
+  echo "The system C/C++ libraries..."
+  find /usr/lib* /lib -name 'libc.*' -o -name 'libstdc++.*' -o -name 'libgcc_s.*'
 }
 
 # Redefine it in the application if more updates are needed.
