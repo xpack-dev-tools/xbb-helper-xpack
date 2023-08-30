dc94ccd676f4250f16149091fad52471bc741a70
Liviu Ionescu <ilg@livius.net>
2023-08-30 16:18:30 +0300

    gcc.sh: fix diff || true

--- dependencies/gcc.sh
+++ dependencies/gcc.sh
@@ -568,7 +568,7 @@ function gcc_build()
               -e "s|^\(POSTSTAGE1_LDFLAGS = .*\)$|\1 -lpthread|" \
               "Makefile"
 
-            run_verbose_develop diff Makefile.bak Makefile
+            run_verbose_develop diff Makefile.bak Makefile || true
           fi
 
           cp "config.log" "${XBB_LOGS_FOLDER_PATH}/${GCC_FOLDER_NAME}/config-log-$(ndate).txt"
