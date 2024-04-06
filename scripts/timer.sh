d1665f79f87b13e90179c701955848e5df59a771
Liviu Ionescu <ilg@livius.net>
2024-04-07 00:48:04 +0300

    timer.sh: show minutes and hours

--- scripts/timer.sh
+++ scripts/timer.sh
@@ -26,16 +26,16 @@ function timer_stop()
   local delta_seconds=$((script_end_second-XBB_SCRIPT_BEGIN_SECOND))
   if [ ${delta_seconds} -lt 100 ]
   then
-    echo "Duration: ${delta_seconds} second(s)"
+    printf "Duration: %d second(s)" ${delta_seconds}
   elif [ ${delta_seconds} -lt 3600 ]
   then
     local delta_minutes=$(((delta_seconds+30)/60))
-    echo "Duration: ${delta_minutes} minute(s)"
+    printf "Duration: %d minute(s)" ${delta_minutes}
   else
     local delta_minutes=$(((delta_seconds+30)/60))
     local delta_hours=$((delta_minutes/60))
     local delta_hour_minutes=$((delta_minutes-(delta_hours*60)))
-    printf "Duration: %dh%02dm\n" ${delta_hours} ${delta_hour_minutes}
+    printf "Duration: %d minute(s) (%dh%02dm)\n" ${delta_minutes} ${delta_hours} ${delta_hour_minutes}
   fi
 }
 
