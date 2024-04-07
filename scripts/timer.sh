04807717327bee610dcb3dbed248bdfddaa7e085
Liviu Ionescu <ilg@livius.net>
2024-04-07 07:35:04 +0300

    timer.sh: show minutes and hours

--- scripts/timer.sh
+++ scripts/timer.sh
@@ -24,18 +24,21 @@ function timer_stop()
   echo "The ${XBB_APPLICATION_DISTRO_NAME} ${XBB_APPLICATION_NAME} (${XBB_APPLICATION_LOWER_CASE_NAME}) project - ${XBB_REQUESTED_TARGET_PLATFORM}-${XBB_REQUESTED_TARGET_ARCH}"
   echo "Script \"$0\" completed at $(date)"
   local delta_seconds=$((script_end_second-XBB_SCRIPT_BEGIN_SECOND))
-  if [ ${delta_seconds} -lt 100 ]
+  if [ ${delta_seconds} -lt 2 ]
   then
-    printf "Duration: %d second(s)" ${delta_seconds}
+    printf "Duration: %d second" ${delta_seconds}
+  elif [ ${delta_seconds} -lt 100 ]
+  then
+    printf "Duration: %d seconds" ${delta_seconds}
   elif [ ${delta_seconds} -lt 3600 ]
   then
     local delta_minutes=$(((delta_seconds+30)/60))
-    printf "Duration: %d minute(s)" ${delta_minutes}
+    printf "Duration: %d minutes" ${delta_minutes}
   else
     local delta_minutes=$(((delta_seconds+30)/60))
     local delta_hours=$((delta_minutes/60))
     local delta_hour_minutes=$((delta_minutes-(delta_hours*60)))
-    printf "Duration: %d minute(s) (%dh%02dm)\n" ${delta_minutes} ${delta_hours} ${delta_hour_minutes}
+    printf "Duration: %d minutes (%dh%02dm)\n" ${delta_minutes} ${delta_hours} ${delta_hour_minutes}
   fi
 }
 
