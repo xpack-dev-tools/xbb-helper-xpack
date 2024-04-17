927f251e5f1d9a49f4fa67369812ceab16bf9dd8
Liviu Ionescu <ilg@livius.net>
2024-04-18 00:15:53 +0300

    gcc.sh: rework libiconv hack

--- dependencies/gcc.sh
+++ dependencies/gcc.sh
@@ -637,7 +637,7 @@ function gcc_build()
           # Hack to include the libiconv.a objects into libstdc++.a.
           local tmp_path=$(mktemp -d)
 
-          if [ "${XBB_TARGET_ARCH}" == "x64" ] || [ "${XBB_TARGET_ARCH}" == "arm64" ]
+          if [ "${XBB_TARGET_ARCH}" == "x64" ]
           then
             (
               rm -rf "${tmp_path}"
@@ -647,10 +647,6 @@ function gcc_build()
               ${AR} xv "${XBB_LIBRARIES_INSTALL_FOLDER_PATH}/static64/lib/libiconv.a"
               ${AR} rsv "${XBB_EXECUTABLES_INSTALL_FOLDER_PATH}/lib64/libstdc++.a" *.o
             )
-          fi
-
-          if [ "${XBB_TARGET_ARCH}" == "x64" ] || [ "${XBB_TARGET_ARCH}" == "arm" ]
-          then
             (
               rm -rf "${tmp_path}"
               mkdir "${tmp_path}"
@@ -659,6 +655,19 @@ function gcc_build()
               ${AR} xv "${XBB_LIBRARIES_INSTALL_FOLDER_PATH}/static32/lib/libiconv.a"
               ${AR} rsv "${XBB_EXECUTABLES_INSTALL_FOLDER_PATH}/lib32/libstdc++.a" *.o
             )
+          elif [ "${XBB_TARGET_ARCH}" == "arm64" ] || [ "${XBB_TARGET_ARCH}" == "arm" ]
+          then
+            (
+              rm -rf "${tmp_path}"
+              mkdir "${tmp_path}"
+              cd "${tmp_path}"
+
+              ${AR} xv "${XBB_LIBRARIES_INSTALL_FOLDER_PATH}/static/lib/libiconv.a"
+              ${AR} rsv "${XBB_EXECUTABLES_INSTALL_FOLDER_PATH}/lib/libstdc++.a" *.o
+            )
+          else
+            echo "Unsupported XBB_TARGET_ARCH=${XBB_TARGET_ARCH} in ${FUNCNAME[0]}()"
+            exit 1
           fi
         fi
 
