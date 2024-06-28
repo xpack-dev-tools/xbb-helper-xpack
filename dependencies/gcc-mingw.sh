8cc6162cbf868164f0ea82bb66026b1c776482b1
Liviu Ionescu <ilg@livius.net>
2024-06-28 16:18:35 +0300

    gcc-mingw.sh: upper case /Work/ kludge

--- dependencies/gcc-mingw.sh
+++ dependencies/gcc-mingw.sh
@@ -97,7 +97,7 @@ function gcc_mingw_build_all_triplets()
     # Set XBB_TARGET_STRIP, _RANLIB & _OBJDUMP
     xbb_set_extra_target_env "${triplet}"
 
-    binutils_build "${XBB_BINUTILS_VERSION}" --triplet="${triplet}" --program-prefix="${triplet}-"
+#    binutils_build "${XBB_BINUTILS_VERSION}" --triplet="${triplet}" --program-prefix="${triplet}-"
 
     # Deploy the headers, they are needed by the compiler.
     mingw_build_headers --triplet="${triplet}"
@@ -928,7 +928,6 @@ function gcc_mingw_test()
     # - The Windows directory.
     # - The PATH variable directories.
 
-
     # Run tests in all cases.
     (
       # For libstdc++-6.dll & co.
@@ -962,7 +961,10 @@ function gcc_mingw_test()
           echo "Cannot get libstdc++.a path"
           exit 1
         fi
-        local cxx_lib_path=$(dirname $(echo ${libstdcpp_file_path} | sed -e 's|[A-Z]:||'))
+        
+        # For unknown reasons, upper case letters are converted to lower case,
+        # thus the need to restore `/Work/`, but this is an ugly kludge.
+        local cxx_lib_path=$(dirname $(echo ${libstdcpp_file_path} | sed -e 's|[a-zA-Z]:||' -e 's|/work/|/Work/|'))
         if [ -f "${cxx_lib_path}/libstdc++-6.dll" ]
         then
           export WINEPATH="$(${REALPATH} ${cxx_lib_path});${WINEPATH:-}"
