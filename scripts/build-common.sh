49b2b8642e6afed9f3242169295d05ea7d87a5a7
Liviu Ionescu <ilg@livius.net>
2024-08-01 22:26:02 +0300

    build-common.sh: update

--- scripts/build-common.sh
+++ scripts/build-common.sh
@@ -348,7 +348,7 @@ function build_common_run()
 
       run_verbose ls -l "${XBB_DEPLOY_FOLDER_PATH}"
 
-      run_verbose tree -L 2 "${XBB_APPLICATION_INSTALL_FOLDER_PATH}" || true
+      run_verbose tree -L 2 -A "${XBB_APPLICATION_INSTALL_FOLDER_PATH}" || true
 
     ) 2>&1 | tee "${XBB_LOGS_FOLDER_PATH}/post-lists-output-$(ndate).txt"
   fi
