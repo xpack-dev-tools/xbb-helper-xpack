59caa237cc889936129f095c76abecb7026ca7b4
Liviu Ionescu <ilg@livius.net>
2024-08-05 15:43:44 +0300

    move-to-build-assets.sh added

---
+++ maintainer-scripts/move-to-build-assets.sh
@@ -0,0 +1,112 @@
+#!/usr/bin/env bash
+
+# -----------------------------------------------------------------------------
+# Safety settings (see https://gist.github.com/ilg-ul/383869cbb01f61a51c4d).
+
+if [[ ! -z ${DEBUG} ]]
+then
+  set ${DEBUG} # Activate the expand mode if DEBUG is anything but empty.
+else
+  DEBUG=""
+fi
+
+set -o errexit # Exit if command failed.
+set -o pipefail # Exit if pipe failed.
+set -o nounset # Exit if variable not set.
+
+# Remove the initial space and instead use '\n'.
+IFS=$'\n\t'
+
+# -----------------------------------------------------------------------------
+# Identify the script location, to reach, for example, the helper scripts.
+
+script_path="$0"
+if [[ "${script_path}" != /* ]]
+then
+  # Make relative path absolute.
+  script_path="$(pwd)/$0"
+fi
+
+script_name="$(basename "${script_path}")"
+
+script_folder_path="$(dirname "${script_path}")"
+script_folder_name="$(basename "${script_folder_path}")"
+
+# =============================================================================
+
+tmp_file="$(mktemp)"
+cat <<'__EOF__' >"${tmp_file}"
+cd "$1/.."
+
+echo
+echo $1
+
+
+if false
+then
+
+  # Skip if already processed.
+  if [ -d build-assets ]
+  then
+    exit 0
+  fi
+
+  # Skip if not a binary xPack.
+  if [ ! -d scripts ]
+  then
+    exit 0
+  fi
+
+  git checkout -b xpack-development
+
+  mkdir build-assets
+  mv build extras node_modules patches scripts wrappers xpacks README-*.md build-assets
+
+  mv README.md README-LONG.md
+  touch README.md
+
+  cp package.json build-assets
+  rm package-lock.json
+
+  xpm run generate-workflows -C build-assets
+
+  xpm run website-generate-commons -C build-assets
+  xpm run website-import-releases -C build-assets
+
+else
+
+  # Skip if build-assets not created.
+  if [ ! -d build-assets ]
+  then
+    exit 0
+  fi
+
+  cd build-assets
+
+  # bring templates up one level
+  mv scripts/templates .
+
+  cd templates
+  mv body-jekyll-release-post-part-1-liquid.md body-blog-release-post-part-1-liquid.md
+  mv body-jekyll-release-post-part-2-liquid.md body-blog-release-post-part-2-liquid.md
+
+fi
+
+__EOF__
+
+# -----------------------------------------------------------------------------
+
+set -x
+
+commands_file="${tmp_file}"
+
+repos_folder="$(dirname $(dirname "${script_folder_path}"))"
+
+cd "${repos_folder}"
+
+find . -type d -name '.git' -print0 | sort -zn | \
+  xargs -0 -I '{}' bash "${commands_file}" '{}'
+
+echo
+
+# -----------------------------------------------------------------------------
