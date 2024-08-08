f86631d4dca9ef3434fa6e79a42195c61a1fdfe4
Liviu Ionescu <ilg@livius.net>
2024-08-09 02:16:36 +0300

    website-generate-commons.sh: showTestsResults

--- maintainer-scripts/website-generate-commons.sh
+++ maintainer-scripts/website-generate-commons.sh
@@ -59,9 +59,9 @@ export appLcName="$(liquidjs --context @package.json --template '{{ xpack.proper
 export platforms="$(liquidjs --context @package.json --template '{{ xpack.properties.platforms }}')"
 export showDeprecatedGnuMcuAnalytics="$(liquidjs --context @package.json --template '{{ xpack.properties.showDeprecatedGnuMcuAnalytics }}')"
 export showDeprecatedRiscvGccAnalytics="$(liquidjs --context @package.json --template '{{ xpack.properties.showDeprecatedRiscvGccAnalytics }}')"
-export showTestResults="$(liquidjs --context @package.json --template '{{ xpack.properties.showTestResults }}')"
+export showTestsResults="$(liquidjs --context @package.json --template '{{ xpack.properties.showTestsResults }}')"
 
-export context="{ \"appName\": \"${appName}\", \"appLcName\": \"${appLcName}\", \"platforms\": \"${platforms}\", \"branch\": \"${branch}\", \"showDeprecatedGnuMcuAnalytics\": \"${showDeprecatedGnuMcuAnalytics}\", \"showDeprecatedRiscvGccAnalytics\": \"${showDeprecatedRiscvGccAnalytics}\", \"showTestResults\": \"${showTestResults}\" }"
+export context="{ \"appName\": \"${appName}\", \"appLcName\": \"${appLcName}\", \"platforms\": \"${platforms}\", \"branch\": \"${branch}\", \"showDeprecatedGnuMcuAnalytics\": \"${showDeprecatedGnuMcuAnalytics}\", \"showDeprecatedRiscvGccAnalytics\": \"${showDeprecatedRiscvGccAnalytics}\", \"showTestsResults\": \"${showTestsResults}\" }"
 
 # tmp_context_file="$(mktemp) -t context"
 # echo "{ \"appName\": \"${appName}\", \"appLcName\": \"${appLcName}\", \"platforms\": \"${platforms}\" }" > "${tmp_context_file}"
