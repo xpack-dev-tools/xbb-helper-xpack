e243d24c44681aca1bfff78133966c0cf3d60421
Liviu Ionescu <ilg@livius.net>
2024-08-09 02:16:08 +0300

    generate-workflows: use platform

--- maintainer-scripts/generate-workflows.sh
+++ maintainer-scripts/generate-workflows.sh
@@ -70,7 +70,15 @@ cd "${root_folder}"
 # Use liquidjs to extract properties from package.json.
 export appName="$(liquidjs --context @package.json --template '{{ xpack.properties.appName }}')"
 export appLcName="$(liquidjs --context @package.json --template '{{ xpack.properties.appLcName }}')"
+
+# "all" is equivalent with "linux-x64,linux-arm64,linux-arm,darwin-x64,darwin-arm64,win32-x64"
 export platforms="$(liquidjs --context @package.json --template '{{ xpack.properties.platforms }}')"
+
+platforms_with_commas=",${platforms},"
+if [ "${platforms_with_commas}" == ",all," ]
+then
+  platforms_with_commas=",linux-x64,linux-arm64,linux-arm,darwin-x64,darwin-arm64,win32-x64,"
+fi
 
 export context="{ \"XBB_APPLICATION_NAME\": \"${appName}\", \"XBB_APPLICATION_LOWER_CASE_NAME\": \"${appLcName}\", \"platforms\": \"${platforms}\", \"prefix\": \"${prefix}\", \"branch\": \"${branch}\" }"
 
@@ -85,15 +93,48 @@ echo
 echo "Workflows..."
 cp -v ${helper_folder}/templates/body-github-pre-releases-test.md ${project_folder}/.github/workflows/
 
-run_verbose liquidjs --context "${context}" --template @${helper_folder}/templates/workflows/build-darwin-x64-liquid.yml --output ${project_folder}/.github/workflows/build-darwin-x64.yml
-run_verbose liquidjs --context "${context}" --template @${helper_folder}/templates/workflows/build-darwin-arm64-liquid.yml --output ${project_folder}/.github/workflows/build-darwin-arm64.yml
-run_verbose liquidjs --context "${context}" --template @${helper_folder}/templates/workflows/build-linux-x64-liquid.yml --output ${project_folder}/.github/workflows/build-linux-x64.yml
-run_verbose liquidjs --context "${context}" --template @${helper_folder}/templates/workflows/build-win32-x64-liquid.yml --output ${project_folder}/.github/workflows/build-win32-x64.yml
-run_verbose liquidjs --context "${context}" --template @${helper_folder}/templates/workflows/build-linux-arm-liquid.yml --output ${project_folder}/.github/workflows/build-linux-arm.yml
-run_verbose liquidjs --context "${context}" --template @${helper_folder}/templates/workflows/build-linux-arm64-liquid.yml --output ${project_folder}/.github/workflows/build-linux-arm64.yml
+if [[ "${platforms_with_commas}" =~ ,darwin-x64, ]]
+then
+  run_verbose liquidjs --context "${context}" --template @${helper_folder}/templates/workflows/build-darwin-x64-liquid.yml --output ${project_folder}/.github/workflows/build-darwin-x64.yml
+fi
+
+if [[ "${platforms_with_commas}" =~ ,darwin-arm64, ]]
+then
+  run_verbose liquidjs --context "${context}" --template @${helper_folder}/templates/workflows/build-darwin-arm64-liquid.yml --output ${project_folder}/.github/workflows/build-darwin-arm64.yml
+fi
+
+if [[ "${platforms_with_commas}" =~ ,linux-x64, ]]
+then
+  run_verbose liquidjs --context "${context}" --template @${helper_folder}/templates/workflows/build-linux-x64-liquid.yml --output ${project_folder}/.github/workflows/build-linux-x64.yml
+fi
+
+if [[ "${platforms_with_commas}" =~ ,win32-x64, ]]
+then
+  run_verbose liquidjs --context "${context}" --template @${helper_folder}/templates/workflows/build-win32-x64-liquid.yml --output ${project_folder}/.github/workflows/build-win32-x64.yml
+fi
+
+if [[ "${platforms_with_commas}" =~ ,linux-arm, ]]
+then
+  run_verbose liquidjs --context "${context}" --template @${helper_folder}/templates/workflows/build-linux-arm-liquid.yml --output ${project_folder}/.github/workflows/build-linux-arm.yml
+fi
+
+if [[ "${platforms_with_commas}" =~ ,linux-arm64, ]]
+then
+  run_verbose liquidjs --context "${context}" --template @${helper_folder}/templates/workflows/build-linux-arm64-liquid.yml --output ${project_folder}/.github/workflows/build-linux-arm64.yml
+fi
+
 run_verbose liquidjs --context "${context}" --template @${helper_folder}/templates/workflows/test-prime-liquid.yml --output ${project_folder}/.github/workflows/test-prime.yml
-run_verbose liquidjs --context "${context}" --template @${helper_folder}/templates/workflows/test-docker-linux-intel-liquid.yml --output ${project_folder}/.github/workflows/test-docker-linux-intel.yml
-run_verbose liquidjs --context "${context}" --template @${helper_folder}/templates/workflows/test-docker-linux-arm-liquid.yml --output ${project_folder}/.github/workflows/test-docker-linux-arm.yml
+
+if [[ "${platforms_with_commas}" =~ ,linux-x64, ]]
+then
+  run_verbose liquidjs --context "${context}" --template @${helper_folder}/templates/workflows/test-docker-linux-intel-liquid.yml --output ${project_folder}/.github/workflows/test-docker-linux-intel.yml
+fi
+
+if [[ "${platforms_with_commas}" =~ ,linux-arm64, ]] || [[ "${platforms_with_commas}" =~ ,linux-arm, ]]
+then
+ run_verbose liquidjs --context "${context}" --template @${helper_folder}/templates/workflows/test-docker-linux-arm-liquid.yml --output ${project_folder}/.github/workflows/test-docker-linux-arm.yml
+fi
+
 run_verbose liquidjs --context "${context}" --template @${helper_folder}/templates/workflows/publish-release-liquid.yml --output ${project_folder}/.github/workflows/publish-release.yml
 run_verbose liquidjs --context "${context}" --template @${helper_folder}/templates/workflows/test-xpm-liquid.yml --output ${project_folder}/.github/workflows/test-xpm.yml
 run_verbose liquidjs --context "${context}" --template @${helper_folder}/templates/workflows/deep-clean-liquid.yml --output ${project_folder}/.github/workflows/deep-clean.yml
