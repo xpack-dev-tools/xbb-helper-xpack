bd1ca2b9435603ba70659630f1def854776d5112
Liviu Ionescu <ilg@livius.net>
2024-10-07 10:16:32 +0300

    docusaurus.config-liquid.ts: /baseUrl/

--- templates/docusaurus/common/docusaurus.config-liquid.ts
+++ templates/docusaurus/common/docusaurus.config-liquid.ts
@@ -88,7 +88,7 @@ const config: Config = {
   url: 'https://xpack-dev-tools.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '{% if customFields.docusaurusBaseUrl %}{{customFields.docusaurusBaseUrl}}{% else %}/{{gitHubProjectName}}{% endif %}',
+  baseUrl: '{% if customFields.docusaurusBaseUrl %}{{customFields.docusaurusBaseUrl}}{% else %}/{{gitHubProjectName}}/{% endif %}',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
