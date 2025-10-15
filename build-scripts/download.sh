e4d1f4cd70d60e5447d8362d657861819efb6c3b
Liviu Ionescu <ilg@livius.net>
2025-10-16 00:59:10 +0300

    download.sh more echo

--- build-scripts/download.sh
+++ build-scripts/download.sh
@@ -135,6 +135,7 @@ function download()
       # Try primary URL up to 3 times
       local success="false"
       for attempt in 1 2 3; do
+        echo
         echo "Attempting download from primary URL (attempt ${attempt}/3)..."
         if run_verbose curl --insecure --fail --location --output "${XBB_DOWNLOAD_FOLDER_PATH}/${archive_name}.${rand}.download" "${url}"
         then
@@ -142,6 +143,7 @@ function download()
           success="true"
           break
         else
+          echo
           echo "Download attempt ${attempt} failed"
           if [ ${attempt} -lt 3 ]; then
             echo "Retrying in 5 seconds..."
@@ -152,6 +154,7 @@ function download()
 
       # If primary URL failed all attempts, try backup URL
       if [ "${success}" == "false" ]; then
+        echo
         echo "All primary URL attempts failed, trying backup URL..."
         local backup_url="https://github.com/xpack-dev-tools/files-mirror/releases/download/binaries/$(basename ${url})"
         echo "Downloading \"${archive_name}\" from backup \"${backup_url}\"..."
