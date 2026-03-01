a9243cbfffbf4c2b28d70908fde67dfb7a01db9a
Liviu Ionescu <ilg@livius.net>
2026-03-01 23:05:09 +0200

    gettext.sh: export LIBS

--- dependencies/gettext.sh
+++ dependencies/gettext.sh
@@ -73,7 +73,7 @@ function gettext_build()
       fi
 
       LDFLAGS="${XBB_LDFLAGS_LIB}"
-      LIBS+=" -liconv"
+      LIBS="-liconv"
 
       xbb_adjust_ldflags_rpath
 
@@ -81,7 +81,8 @@ function gettext_build()
       export CFLAGS
       export CXXFLAGS
       export LDFLAGS
-
+      export LIBS
+      
       if [ ! -f "config.status" ]
       then
         (
